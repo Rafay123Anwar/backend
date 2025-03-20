62916465e1d460582045b1d18494b0872e564610
[Rafay123Anwar] <[muhammadrafayy0@gmail.com]>
2025-03-21 00:42:41 +0500

    first commit

--- freelance_platform/wsgi.py
+++ freelance_platform/wsgi.py
@@ -1,7 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'freelance_platform.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "freelance_platform.settings")
 
 application = get_wsgi_application()
-app=application()
